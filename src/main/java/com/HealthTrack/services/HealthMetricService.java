6c88d340c1e562d846767fa0854b92d55af92039
Candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2024-09-19 16:06:46 -0500

    added createHealthMetric in serivces

---
+++ src/main/java/com/HealthTrack/services/HealthMetricService.java
@@ -0,0 +1,10 @@
+package com.HealthTrack.services;
+
+import com.HealthTrack.dtos.HealthMetricDto;
+
+public interface HealthMetricService {
+
+    HealthMetricDto createHealthMetric(HealthMetricDto healthMetricDto);
+
+
+}
---
+++ src/main/java/com/HealthTrack/services/imp/HealthMetricServiceImpl.java
@@ -0,0 +1,24 @@
+package com.HealthTrack.services.imp;
+
+import com.HealthTrack.dtos.HealthMetricDto;
+import com.HealthTrack.mapper.HealthMetricMapper;
+import com.HealthTrack.models.HealthMetric;
+import com.HealthTrack.repositories.HealthMetricRepository;
+import com.HealthTrack.services.HealthMetricService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
+public class HealthMetricServiceImpl implements HealthMetricService {
+
+    private HealthMetricRepository healthMetricRepository;
+
+    @Override
+    public HealthMetricDto createHealthMetric(HealthMetricDto healthMetricDto) {
+
+        HealthMetric healthMetric = HealthMetricMapper.mapToHealthMetric(healthMetricDto);
+        HealthMetric saveHealthMetric = healthMetricRepository.save(healthMetric);
+        return HealthMetricMapper.mapToHealthMetricDto(saveHealthMetric);
+    }
+}
