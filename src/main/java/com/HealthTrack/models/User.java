b08b12bcdbe706ecab37b93a39ee5246abd4bd57
Candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2024-09-19 09:24:26 -0500

    adding role_user and role_ad

--- src/main/java/com/HealthTrack/models/User.java
+++ src/main/java/com/HealthTrack/models/User.java
@@ -5,6 +5,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Entity
 @Table(name = "users")
@@ -22,5 +25,8 @@ public class User {
 
     @Column(nullable = false)
     private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    private List<String> roles = new ArrayList<>();
 
 }
