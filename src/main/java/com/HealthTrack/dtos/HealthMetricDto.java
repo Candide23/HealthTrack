0fb12c9d707b5095561b411867d2d79291a7c435
Candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2024-09-19 15:34:02 -0500

    added HealthMetric dto

---
+++ src/main/java/com/HealthTrack/dtos/HealthMetricDto.java
@@ -0,0 +1,21 @@
+package com.HealthTrack.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class HealthMetricDto {
+
+
+    private Long id;
+    private String metricType;
+    private Double value;
+    private LocalDateTime timestamp;
+}
