bd1a090f8ae697eb669dccdac40b235eb29796d6
Candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2024-09-20 19:37:25 -0500

    added symtom controllers

---
+++ src/main/java/com/HealthTrack/controllers/SymptomController.java
@@ -0,0 +1,48 @@
+package com.HealthTrack.controllers;
+
+
+import com.HealthTrack.dtos.SymptomDto;
+import com.HealthTrack.services.SymptomService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/symptoms")
+public class SymptomController {
+
+    private SymptomService symptomService;
+
+
+
+    @PostMapping
+    public ResponseEntity<SymptomDto> createSymptom(@RequestBody SymptomDto symptomDto) {
+        SymptomDto createdSymptom = symptomService.createSymptom(symptomDto);
+        return new ResponseEntity<>(createdSymptom, HttpStatus.CREATED);
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<SymptomDto> getSymptomById(@PathVariable("id") Long idSymptom) {
+        SymptomDto symptom = symptomService.findSymptomById(idSymptom);
+        return ResponseEntity.ok(symptom);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<SymptomDto>> getAllSymptoms() {
+        List<SymptomDto> symptoms = symptomService.findAllSymptom();
+        return new ResponseEntity<>(symptoms, HttpStatus.OK);
+    }
+
+
+    // UPDATE
+    @PutMapping("/{id}")
+    public ResponseEntity<SymptomDto> updateSymptom(@PathVariable Long id, @RequestBody SymptomDto symptomDto) {
+        SymptomDto updatedSymptom = symptomService.updateSymptom(id, symptomDto);
+        return ResponseEntity.ok(updatedSymptom);
+    }
+}
