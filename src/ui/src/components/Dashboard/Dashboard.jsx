6c89dc14406ed131b9718bab5fda755b2cea1850
Candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2024-09-22 16:01:16 -0500

    added Dashboard component

---
+++ src/ui/src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div className="container mt-5 text-center">
+      <h1>Welcome to MediTrack</h1>
+      <p>Manage your health by tracking metrics, symptoms, and scheduling appointments.</p>
+    </div>
+  );
+};
+
+export default Dashboard;
