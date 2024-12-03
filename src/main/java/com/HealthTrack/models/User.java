406dc6174ec0cb2aadb723ccd08f7ea65b0142be
Candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2024-12-03 08:23:49 -0600

    deleted spring security

--- src/main/java/com/HealthTrack/models/User.java
+++ src/main/java/com/HealthTrack/models/User.java
@@ -48,14 +48,7 @@ public class User {
     @JsonIgnore
     private List<Notification> notifications;
 
-    // New field for roles
-    @ManyToMany(fetch = FetchType.EAGER, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
-    @JoinTable(
-            name = "user_roles",
-            joinColumns = @JoinColumn(name = "user_id"),
-            inverseJoinColumns = @JoinColumn(name = "role_id")
-    )
-    private Set<Role> roles;  // Roles assigned to the user
+
 
 
 
